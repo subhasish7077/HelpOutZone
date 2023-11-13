e287235f66cb02f92476baf7740a432c06666bc4
Subhasish <subhasishpanda7077@gmail.com>
2023-11-13 21:38:04 +0530

    filter and search added to question lists

--- QandA/views.py
+++ QandA/views.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.shortcuts import redirect, render
 from django.db.models.functions import Length
 from django.db.models import Count
@@ -8,7 +9,6 @@ from django.http import JsonResponse, HttpResponseRedirect
 
 def get_questionByID(request,pk):
     question = Question.objects.filter(id=pk)[0]
-    
     def update_views(question):
         if question.views.filter(id = request.user.id).exists():
             pass
@@ -21,22 +21,25 @@ def get_questionByID(request,pk):
 def Questionsview(request):
     tag_categories = list(TagCategory.objects.all())
     tag_categories.sort(key=lambda x:x.name)
-    sort_by = request.GET.get('sort_by') if request.GET.get('sort_by') is not None else '-created_at'
+    questions = Question.objects.all()
+    search_text = request.GET.get('question_search') or ""
+    if search_text:
+        questions = questions.filter(title__icontains=search_text)
+    tags = request.GET.getlist('tags')
+    sort_by = request.GET.get('sort_by') or '-created_at'
+    if tags:
+        questions = questions.filter(tags__id__in=tags).distinct()
     if sort_by in ['views', 'votes']:
-        questions = Question.objects.annotate(total=Count(sort_by)).order_by('-total')
+        questions = questions.annotate(total=Count(sort_by)).order_by('-total')
     else:
-        questions = Question.objects.all().order_by(sort_by)
-    return render(request, 'QandA.html', {'tag_categories':tag_categories,'questions':questions ,'sort':sort_by})
+        questions=questions.order_by(sort_by)
+    return render(request, 'QandA.html', {'tag_categories':tag_categories,'questions':questions ,'sort':sort_by,'search':search_text})
 
 def get_tags(request):
     category_id = request.GET.get('category_id')
-    # Retrieve tags based on the selected category
     if category_id:
         tags = Tag.objects.filter(category=category_id).values('id', 'name')
     else:
-        # If no category is selected, return all tags
         tags = Tag.objects.values('id', 'name')
-    # Convert the queryset to a list of dictionaries
     tags_list = list(tags)
-    # Return a JSON response
     return JsonResponse({'tags': tags_list})
