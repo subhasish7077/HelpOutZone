fc2d62b9a3232a43e346cf3897bea1d53e57c8a1
Subhasish <subhasishpanda7077@gmail.com>
2023-11-18 21:46:10 +0530

    added delete and some content

--- QandA/urls.py
+++ QandA/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('QandA/<pk>',views.get_questionByID,name='Question_byid'),
     path('QandA/',views.Questionsview,name='questions_list'),
     path('get_tags/', views.get_tags, name='get_tags'),
+    path('QandA/tag/<pk>/', views.QuestionsByTags, name='get_question_by_tags'),
+    path('QandA/answer/delete/', views.deleteanswer, name='delete_answer'),
 ]
