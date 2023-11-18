1c6f0f97d36546ef8a1f7144eeacf92889218177
aditi1145 <aditidas1145@gmail.com>
2023-11-18 23:07:06 +0530

    msg

--- notes/urls.py
+++ notes/urls.py
@@ -1,9 +1,11 @@
 # urls.py
 from django.urls import path
-from .views import upload_file, file_list, download_file
+from .views import file_upload, file_list, search_posts,download_file
 app_name='notes'
 urlpatterns = [
-    path('upload/', upload_file, name='upload_file'),
+    path('upload/', file_upload, name='file_upload'),
     path('files/', file_list, name='file_list'),
     path('download/<int:file_id>/', download_file, name='download_file'),
+    path('search/', search_posts, name='search_posts'),
+
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,30 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import UploadedFile
+from .models import FileUpload
 from .forms import FileUploadForm
 from django.http import HttpResponse
-
-
-def upload_file(request):
-    if request.method == 'POST':
-        form = FileUploadForm(request.POST, request.FILES)
-        if form.is_valid():
-            file_instance = form.save(commit=False)
-            file_instance.user = request.user
-            file_instance.save()
-            return redirect('notes:file_list')
-    else:
-        form = FileUploadForm()
-    return render(request, 'upload_file.html', {'form': form})
-
-
-def file_list(request):
-    files = UploadedFile.objects.filter(user=request.user)
-    return render(request, 'file_list.html', {'files': files})
-
 
 def download_file(request, file_id):
     file = UploadedFile.objects.get(pk=file_id)
     response = HttpResponse(file.file, content_type='application/force-download')
     response['Content-Disposition'] = f'attachment; filename="{file.file.name}"'
     return response
+
+
+def file_upload(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_upload = form.save(commit=False)
+            file_upload.user = request.user
+            file_upload.save()
+            return redirect('file_list')
+    else:
+        form = FileUploadForm()
+    return render(request, 'file_upload.html', {'form': form})
+
+
+def file_list(request):
+    files = FileUpload.objects.all()
+    return render(request, 'file_list.html', {'files': files})
+
+
+def search_posts(request):
+    if 'tag' in request.GET:
+        tag = request.GET['tag']
+        files = FileUpload.objects.filter(tags__icontains=tag)
+    else:
+        files = FileUpload.objects.all()
+    return render(request, 'file_list.html', {'files': files})
