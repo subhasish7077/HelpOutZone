9b03776dbdcc3437ead539801eb119f421924c9d
aditi1145 <aditidas1145@gmail.com>
2023-11-20 17:10:11 +0530

    changed base files

--- base_app/urls.py
+++ base_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+
 app_name = 'base_app'
 urlpatterns = [
     path('',views.home,name='home page'),
     path('about/',views.about,name='about'),
+
     
 ]
