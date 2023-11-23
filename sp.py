d0e4b97c7de156b54608041040f66e7ac41ba3ad
subhasish7077 <83899542+subhasish7077@users.noreply.github.com>
2023-11-24 04:44:08 +0530

    Update sp.py

--- sp.py
+++ sp.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = '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'
+openai.api_key = 
 
 response = openai.Completion.create(
         model="gpt-3.5-turbo-instruct",
